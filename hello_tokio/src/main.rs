3e5363f8c34260140a1423143be262321b7d21cf
shobhitsundriyal <shobhitsundriyal70@gmail.com>
2024-10-31 12:06:18 +0530

    3.2 passed argument attribute to run in single threaded mode

--- hello_tokio/src/main.rs
+++ hello_tokio/src/main.rs
@@ -6,8 +6,8 @@ async fn hello() {
 
 // ok so if we know that our whole program is async, we can use tokio's simple syntax, eg: in case of a server, whole server is async
 // by default, tokio::main will create a runtime for us with multi-threaded executor and max performance settings
-// but we can pass our own settings as well
-# [tokio::main]
+// but we can pass our own settings as well, inside the attribute
+# [tokio::main(flavor = "current_thread")]
 async fn main() {
     // with this we can make the main function async and simply await the async functions
     hello().await;
