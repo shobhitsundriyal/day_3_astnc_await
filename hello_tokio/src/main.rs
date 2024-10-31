991a310ec19cdd518ccccff4d3bcd1561f61fc0c
shobhitsundriyal <shobhitsundriyal70@gmail.com>
2024-10-31 12:04:40 +0530

    3.2 little comments added

--- hello_tokio/src/main.rs
+++ hello_tokio/src/main.rs
@@ -5,6 +5,8 @@ async fn hello() {
 }
 
 // ok so if we know that our whole program is async, we can use tokio's simple syntax, eg: in case of a server, whole server is async
+// by default, tokio::main will create a runtime for us with multi-threaded executor and max performance settings
+// but we can pass our own settings as well
 # [tokio::main]
 async fn main() {
     // with this we can make the main function async and simply await the async functions
